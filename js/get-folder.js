051dfa779177a5a9d37f40bc7947823c82caf2d2
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-12-07 14:54:08 +0100

    feat(getFolder): get the list of files inside the selected folder

--- js/get-folder.js
+++ js/get-folder.js
@@ -3,6 +3,15 @@ import terminalKit from "terminal-kit";
 
 const term = terminalKit.terminal;
 
+const getFileList = async path => {
+	const dir = await fs.promises.opendir(path);
+	term.magenta(`\nThere are files inside:`);
+	for await (const dirent of dir) {
+		term.green(`\n${dirent.name}`);
+	}
+};
+
+
 const getFolder = () => {
 	term.magenta("Enter the path to your folder: ");
 
@@ -11,12 +20,13 @@ const getFolder = () => {
 			console.log("Error:", error);
 			process.exit();
 		} else {
-			(path => {
+			(async path => {
 				const isFolder = fs.lstatSync(path).isDirectory();
 				if (!isFolder) {
 					process.exit();
 				} else {
 					term.green("\nYour folder selected is '%s'\n", path);
+					await getFileList(path);
 				}
 			})(input);
 		}
