517aa56e3fa6bcffabf36d6a144ae7e13522f2a6
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-12-07 13:38:31 +0100

    feat(getFolder): ask the user for a path and check that it is a folder

--- js/get-folder.js
+++ js/get-folder.js
@@ -1,5 +1,26 @@
+import fs from "fs";
+import terminalKit from "terminal-kit";
+
+const term = terminalKit.terminal;
+
 const getFolder = () => {
-	console.log("Created function getFolder()");
+	term.magenta("Enter the path to your folder: ");
+
+	term.inputField((error, input) => {
+		if (error) {
+			console.log("Error:", error);
+			process.exit();
+		} else {
+			(path => {
+				const isFolder = fs.lstatSync(path).isDirectory();
+				if (!isFolder) {
+					process.exit();
+				} else {
+					term.green("\nYour folder selected is '%s'\n", path);
+				}
+			})(input);
+		}
+	});
 };
 
 export { getFolder };
